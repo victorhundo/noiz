dd6d4a839eb87b75ab43389367ea138db41fc4d0
victorhundo <victorhundo@gmail.com>
2019-09-02 11:21:08 -0300

    hot fix

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Component({
