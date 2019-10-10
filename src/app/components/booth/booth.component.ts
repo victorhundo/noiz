6e8bf72f9ec689af9d1ceb279d7a5220f887564e
victorhundo <victorhundo@gmail.com>
2019-10-10 17:37:24 -0300

    Refatorado cabine de votação

--- src/app/components/booth/booth.component.ts
+++ src/app/components/booth/booth.component.ts
@@ -1,100 +1,119 @@
 import { Component, OnInit } from '@angular/core';
 import { Router,ActivatedRoute,ParamMap } from '@angular/router';
 import { ElectionService } from 'src/app/services/election.service';
-import {FormBuilder, FormGroup, Validators, FormArray} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormArray, SelectMultipleControlValueAccessor} from '@angular/forms';
 import { Observable } from 'rxjs';
 import { EncryptedVote } from 'src/app/models/encryptedVote';
 import unescapeJs from 'unescape-js';
-declare var b64_sha256:any;
-declare var BigInt:any; 
+import { MatStepper } from '@angular/material';
+import { sortObj } from 'sort-object';
+
+declare var b64_sha256: any;
+declare var BigInt: any;
 
 @Component({
   selector: 'app-booth',
   templateUrl: './booth.component.html',
-  styleUrls: ['./booth.component.scss'] 
+  styleUrls: ['./booth.component.scss']
 })
 export class BoothComponent implements OnInit {
-  short_name: string;
+  shortName: string;
   election: any = {};
   questions: any = [];
   eg: any;
   pk: any = {};
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  encryptedBooth: any;
+  encryptedVoteHash: any;
+  encrypting = false;
 
-  constructor( private _formBuilder: FormBuilder, private electionService: ElectionService, private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private electionService: ElectionService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit() {
-    this.short_name = this.route.snapshot.paramMap.get('short_name');
-    this.getElection()
-    this.firstFormGroup = this._formBuilder.group({
+    this.shortName = this.route.snapshot.paramMap.get('short_name');
+    this.getElection();
+    this.firstFormGroup = this.formBuilder.group({
       answer: ['', Validators.required]
     });
-    this.secondFormGroup = this._formBuilder.group({
+    this.secondFormGroup = this.formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
   }
 
-  private normalizeUnicode(a:string) {
-    a = unescapeJs(a);
-    a = a.replace(/u'/g,'\'').replace(/'/g,'"');
-    var result:any = []
-    JSON.parse(decodeURIComponent(a)).forEach(e => {
-      result.push({
-        "answer_urls":e.answer_urls,
-        "answers": e.answers,
-        "choice_type": e.choice_type,
-        "max": e.max,
-        "min": e.min,
-        "question": e.question,
-        "result_type": e.result_type,
-        "tally_type": e.tally_type
-      })
-    });
-    return result
+  goForward(stepper: MatStepper, isDisable?: boolean) {
+    if (!isDisable) { stepper.next(); }
   }
 
-  private getQuestions(c:any){
-    var questions:any = []
+  goBack(stepper: MatStepper, isDisable?: boolean) {
+    if (!isDisable) { stepper.previous(); }
+  }
+
+  private normalizeUnicode(a: string) {
+    a = unescapeJs(a);
+    a = a.replace(/u'/g, '\'').replace(/'/g, '"');
+    const result: any = [];
+    JSON.parse(decodeURIComponent(a)).forEach((e: any) => {
+      result.push({
+        answer_urls: e.answer_urls,
+        answers: e.answers,
+        choice_type: e.choice_type,
+        max: e.max,
+        min: e.min,
+        question: e.question,
+        result_type: e.result_type,
+        tally_type: e.tally_type
+      });
+    });
+    return result;
+  }
+
+  private getQuestions(c: any) {
+    const questions: any = [];
     c.forEach(q => {
-      var answer_urls:any = []
-      var answers:any = []
- 
+      const answerUrls: any = [];
+      const theAnswers: any = [];
+
       q.answer_urls.forEach(e => {
-        answer_urls.push(e.answer_urls)
+        answerUrls.push(e.answer_urls)
       });
-  
+
       q.answers.forEach(e => {
-        answers.push(e.answer)
+        theAnswers.push(e.answer)
       });
-  
+
       questions.push( {
-        "answer_urls":answer_urls,
-          "answers": answers,
-          "choice_type": q.choice_type,
-          "max": q.max,
-          "min": q.min,
-          "question": q.question,
-          "result_type": q.result_type,
-          "tally_type": q.tally_type
-      })      
+        answer_urls: answerUrls,
+        answers: theAnswers,
+        choice_type: q.choice_type,
+        max: q.max,
+        min: q.min,
+        question: q.question,
+        result_type: q.result_type,
+        tally_type: q.tally_type
+      });
     });
     return questions;
   }
 
-  private dateTimeFormat(a:string){
-    a = a.replace('.000000','')
-    return a.slice(0,(a.length-2)) + ':' + a.slice(-2);
+  private dateTimeFormat(a: string) {
+    a = a.replace('.000000', '');
+    return a.slice(0, (a.length - 2)) + ':' + a.slice(-2);
   }
 
-  getElection(){
-    var results: Observable<any> = this.electionService.getElection(this.short_name);
+  getElection() {
+    const results: Observable<any> = this.electionService.getElection(this.shortName);
     results.subscribe( res => {
        this.election = res;
        this.election.frozen_at = this.dateTimeFormat(res.frozen_at);
        this.election.voting_ends_at = this.dateTimeFormat(res.voting_ends_at); 
        this.election.voting_starts_at = this.dateTimeFormat(res.voting_starts_at);
-       var key = JSON.parse(res.public_key);
+       const key = JSON.parse(res.public_key);
        this.election.questions = this.normalizeUnicode(this.election.questions);
        this.questions = this.getQuestions(this.election.questions);
        this.pk.g = key.g;
@@ -105,67 +124,103 @@ export class BoothComponent implements OnInit {
        this.election.hash = b64_sha256(this.toUnicode(JSON.stringify(this.election)));
        this.election.election_hash = this.election.hash;
        this.election.public_key = this.keyBigInt(this.pk);
-    })
+    });
   }
 
-  toUnicode(s:string) {
+  toUnicode(s: string) {
     return s.replace(/[\u007F-\uFFFF]/g, (chr) => {
-      return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substr(-4)
-    })
+      return '\\u' + ('0000' + chr.charCodeAt(0).toString(16)).substr(-4);
+    });
   }
 
-  keyBigInt(s:any){
+  keyBigInt(s: any) {
     return {
-      "g": new BigInt(s.g),
-      "p": new BigInt(s.p),
-      "q": new BigInt(s.q),
-      "y": new BigInt(s.y),
-    } 
+      g: new BigInt(s.g),
+      p: new BigInt(s.p),
+      q: new BigInt(s.q),
+      y: new BigInt(s.y),
+    };
   }
 
-  getAnswer(){
-    return parseInt(this.firstFormGroup.get('answer').value);
+  getAnswer() {
+    return parseInt(this.firstFormGroup.get('answer').value, 10);
   }
 
-  submit() { 
-    var data:string = this.encrypted_vote();
-    console.log(data);
-    var results: Observable<any> = this.electionService.voteElection(this.election.uuid, data);
+  submit() {
+    const results: Observable<any> = this.electionService.voteElection(this.election.uuid, this.encryptedBooth);
     results.subscribe( res => {
-        if(res.status == 200) this.router.navigate([''])
-    })  
+        if (res.status === 200) { this.router.navigate(['']); }
+    });
+  }
+
+  doEncrypted(stepper: MatStepper) {
+    this.encrypting = true;
+    this.encryptedBooth = this.encrypted_vote();
+    this.encryptedVoteHash = b64_sha256(this.encryptedBooth);
+    this.goForward(stepper);
+    this.encrypting = false;
   }
 
   encrypted_vote() {
-    var encryptedBooth = new EncryptedVote([[this.getAnswer()]],this.election); 
+    const encryptedBooth = new EncryptedVote([[this.getAnswer()]], this.election);
     delete encryptedBooth.election;
     encryptedBooth.encrypted_answers.forEach(element => {
+      const individualProofsSorted: any = [];
+      const overallProofSorted: any = [];
       delete element.answer;
       delete element.randomness;
-      element.choices.forEach(c => {
-        c.alpha = c.alpha.toString(),
-        c.beta = c.beta.toString()
+
+      element.choices.forEach((c: any) => {
+        c.alpha = c.alpha.toString();
+        c.beta = c.beta.toString();
         delete c.pk;
       });
-      element.individual_proofs.forEach(c => {
-        c.forEach(p => {
-          p.challenge = p.challenge.toString(),
-          p.commitment.A = p.commitment.A.toString(),
-          p.commitment.B = p.commitment.B.toString(),
-          p.response = p.response.toString()
-        })
+
+      element.individual_proofs.forEach((c: any) => {
+        const proofSorted: any = [];
+        c.forEach((p: any) => {
+          p.challenge = p.challenge.toString();
+          p.commitment.A = p.commitment.A.toString();
+          p.commitment.B = p.commitment.B.toString();
+          p.response = p.response.toString();
+          proofSorted.push({
+            challenge: p.challenge,
+            commitment: {
+              A: p.commitment.A,
+              B: p.commitment.B,
+            },
+            response: p.response
+          });
+        });
+        individualProofsSorted.push(proofSorted);
       });
-      element.overall_proof.forEach(p => {
-        p.challenge = p.challenge.toString(),
-          p.commitment.A = p.commitment.A.toString(),
-          p.commitment.B = p.commitment.B.toString(),
-          p.response = p.response.toString()
+
+
+      element.overall_proof.forEach((p: any) => {
+        p.challenge = p.challenge.toString();
+        p.commitment.A = p.commitment.A.toString();
+        p.commitment.B = p.commitment.B.toString();
+        p.response = p.response.toString();
+        overallProofSorted.push({
+          challenge: p.challenge,
+          commitment: {
+            A: p.commitment.A,
+            B: p.commitment.B,
+          },
+          response: p.response
+        });
       });
+
+      element.individual_proofs = individualProofsSorted;
+      element.overall_proof = overallProofSorted;
+
     });
     return JSON.stringify({
-      "answers": encryptedBooth["encrypted_answers"],
-      "election_hash": encryptedBooth["election_hash"],
-      "election_uuid": encryptedBooth["election_uuid"]
-    })
+      answers: encryptedBooth.encrypted_answers,
+      election_hash: encryptedBooth.election_hash,
+      election_uuid: encryptedBooth.election_uuid
+    });
   }
+
+
 }
